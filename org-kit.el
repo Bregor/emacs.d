62d5925aa5c7644a8598a01d73002579df79752d
Maxim Filatov <pipopolam@gmail.com>
2012-06-30 21:28:01 +0400

    org-kit improvements: * Use ido in org-completion * added some org-modules

--- org-kit.el
+++ org-kit.el
@@ -8,6 +8,9 @@
 	   (setq org-agenda-files (list "~/.emacs.d/org/i-free.org" "~/.emacs.d/org/idleness.org" "~/.emacs.d/org/home.org"))
 	   (setq org-mobile-inbox-for-pull "~/.emacs.d/org/flagged.org")
 	   (setq org-support-shift-select t)
+	   (setq org-completion-use-ido t)
+	   (setq org-modules (quote (org-bbdb org-bibtex org-docview org-info org-jsinfo org-irc org-mac-message org-vm org-collector org-depend org-mac-iCal org-mac-link-grabber org-toc)))
+
 	   ))
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
