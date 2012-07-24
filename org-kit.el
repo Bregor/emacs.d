f8a4985e01d05faf0b4a6a61113232c961ae9f6c
Maxim Filatov <pipopolam@gmail.com>
2012-07-24 19:18:53 +0400

    Org-mode: Added RESEARCH keyword

--- org-kit.el
+++ org-kit.el
@@ -10,7 +10,7 @@
 	   (setq org-support-shift-select t)
 	   (setq org-completion-use-ido t)
 	   (setq org-modules (quote (org-bbdb org-bibtex org-docview org-info org-jsinfo org-irc org-mac-message org-vm org-collector org-depend org-mac-iCal org-mac-link-grabber org-toc)))
-	   (setq org-todo-keywords '((sequence "TODO(t)" "|" "DONE(d)")
+	   (setq org-todo-keywords '((sequence "TODO(t)" "RESEARCH(r)" "|" "DONE(d)")
 				     (sequence "PHONE(p)" "MEETING(m)" "ERRAND(e)" "|" "ENDED(n)")
 				     (sequence "FEEDBACK(f)" "VERIFY(v)" "DELEGATED(g)" "|" "CLOSED(c)")
 				     (sequence "|" "CANCELLED(l)")))
