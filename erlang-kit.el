0a009b20adb2eb38bcfbc45191c51d63436af684
Maxim Filatov <pipopolam@gmail.com>
2012-09-25 03:41:44 +0400

    erlang-kit: rebar files auto-mode

--- erlang-kit.el
+++ erlang-kit.el
@@ -1,3 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.app.src$" . erlang-mode))
+(add-to-list 'auto-mode-alist '("rebar.config" . erlang-mode))
+(add-to-list 'auto-mode-alist '("reltool.config" . erlang-mode))
 
 (provide 'erlang-kit)
