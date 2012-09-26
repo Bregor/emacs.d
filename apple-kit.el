0c4239a1db0ab4705306acf830cf9c3f85d0245a
Maxim Filatov <pipopolam@gmail.com>
2012-09-26 17:40:26 +0400

    apple-kit: use true inconsolata

--- apple-kit.el
+++ apple-kit.el
@@ -1,6 +1,7 @@
 (when (string-match "apple-darwin" system-configuration)
   (setq mac-allow-anti-aliasing t)
-  (set-face-font 'default "-apple-inconsolata-medium-r-normal--12-0-72-72-m-0-iso10646-1")
+  (set-face-font 'default "-apple-Inconsolata-medium-normal-normal-*-18-*-*-*-m-0-iso10646-1")
+  (set-face-font 'bold "-apple-Inconsolata-bold-normal-normal-*-18-*-*-*-m-0-iso10646-1")
   (global-set-key [(backtab)] 'hippie-expand)
   (global-set-key [s-right] 'move-end-of-line)
   (global-set-key [s-left] 'move-beginning-of-line)
