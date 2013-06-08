5bb93b533cc23b418ef01b0e23daa94b48dd6733
Maxim Filatov <pipopolam@gmail.com>
2013-06-09 01:51:36 +0400

    Ruby kit: add ruby-electric-mode, inf-ruby update

--- ruby-kit.el
+++ ruby-kit.el
@@ -3,7 +3,9 @@
      ;; work around possible elpa bug
      (ignore-errors (require 'ruby-compilation))
      (setq ruby-use-encoding-map nil)
-     (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+     (add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings)
+     (add-hook 'ruby-mode-hook
+	       (lambda () (ruby-electric-mode t)))
      (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
      (define-key ruby-mode-map (kbd "C-c l") "lambda")))
